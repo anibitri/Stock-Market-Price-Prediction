abe72f9254a26a846a8c06d2c938e5940d5009bd
anibitri <anibitri7@gmail.com>
2025-04-01 22:07:06 +0100

    updated API and data scraping

--- StockMarketPred.py
+++ StockMarketPred.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import yfinance as yf
 import datetime
-from datetime import timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import regularizers
@@ -15,22 +14,36 @@ from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib.dates import DateFormatter
 import matplotlib.ticker as mticker
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QMainWindow, QLineEdit, QSpinBox, QCheckBox, QGroupBox, QFormLayout
-from PyQt5.QtCore import QSize, Qt
-import sys
-from PyQt5 import QtWidgets
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
 
-# Get the previous business day
-def get_previous_business_day():
-    today = datetime.datetime.now().date()
-    one_day = timedelta(days=1)
-    previous_day = today - one_day
-    while previous_day.weekday() >= 5: 
-        previous_day -= one_day
-    return previous_day.strftime("%Y-%m-%d")
 
-# Data preprocessing functions
+
+#Gather the desired stock financial data from yfinance API
+Symobl = input("Stock Symbol: ")
+symbolTicker = yf.Ticker(Symobl)
+incStat = symbolTicker.get_income_stmt().transpose()
+print(incStat)
+totalRevenue = incStat['TotalRevenue']
+datesIncStat = totalRevenue.index
+datesIncStat = [str(date.date()) for date in datesIncStat]
+data = yf.download(Symobl, period="max")
+
+
+
+#Global Variables
+prediction_days = 100
+future_prediction_days = 10 
+now = datetime.datetime.now().date().strftime('%Y-%m-%d')
+scaled = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaled.fit_transform(data['Close'].values.reshape(-1, 1))
+
+
+#Data Preprocessing
+y = data['Close']
+x = data.drop(['Close'], axis=1)
 def str_to_dt(s):
     split = s.split('-')
     year, month, day = int(split[0]), int(split[1]), int(split[2])
@@ -87,6 +100,9 @@ def data_to_window_data(dataframe, firstdate, lastdate, n=3):
 
     return ret_df
 
+window_df = data_to_window_data(data, '2023-01-01', now, n=3)
+
+
 def window_data_to_xy(window_df):
     data_as_np = window_df.to_numpy()
     dates = data_as_np[:, 0]
@@ -95,7 +111,27 @@ def window_data_to_xy(window_df):
     y = data_as_np[:, -1]
     return dates, x.astype(np.float32), y.astype(np.float32)
 
-# Predict the next 10 days of stock prices
+dates, x, y = window_data_to_xy(window_df)
+
+
+#Split the data into training, validation, and test sets
+q80 = int(len(x)*0.85)
+q90 = int(len(x)*0.92)
+
+dates_train, x_train, y_train = dates[:q80], x[:q80], y[:q80]
+dates_val, x_val, y_val = dates[q80:q90], x[q80:q90], y[q80:q90]
+dates_test, x_test, y_test = dates[q90:], x[q90:], y[q90:]
+
+#Model training using LSTM
+model = Sequential([layers.Input((3,1)), layers.LSTM(64), layers.Dense(32, activation = 'relu'), layers.Dense(32, activation = 'relu'), layers.Dense(1)])
+model.compile(loss='mse', optimizer = Adam(learning_rate = 0.001), metrics = ['mean_absolute_error'])
+model.fit(x_train, y_train, validation_data = (x_val, y_val), epochs = 150)
+
+train_pred = model.predict(x_train).flatten()
+val_pred = model.predict(x_val).flatten()
+test_pred = model.predict(x_test).flatten()
+
+#Predict the next 10 days of stock prices
 def predict_future_prices(model, last_window, num_predictions):
     predictions = []
     current_input = deepcopy(last_window)
@@ -107,148 +143,52 @@ def predict_future_prices(model, last_window, num_predictions):
     
     return predictions
 
-def predict_future_business_days(future_predictions, future_prediction_days, dates_test):
-    # Generate a range of future business dates
-    future_business_dates = pd.bdate_range(start=dates_test[-1], periods=future_prediction_days+1, freq='B')[1:]
-    future_business_dates = [date.strftime('%m-%d') for date in future_business_dates]
-    
-    # Print the future predicted prices
-    for i, pred in enumerate(future_predictions):
-        print(f"Predicted price for {future_business_dates[i]}: {pred}")
-    
-    return future_business_dates
+# Predict the next 10 days of stock prices
+last_window = x_test[-1] 
+future_predictions = predict_future_prices(model, last_window, future_prediction_days)
 
-# Data visualization in PyQt window
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+# Print the future predicted prices
+future_dates = pd.date_range(start=dates_test[-1], periods=future_prediction_days+1, freq='D')[1:]
+future_dates = [date.strftime('%m-%d') for date in future_dates]
+for i, pred in enumerate(future_predictions):
+    print(f"Predicted price for {future_dates[i]}: {pred}")
 
-    def initUI(self):
-        self.setWindowTitle('Stock Market Price Prediction')
-        self.setGeometry(100, 100, 1200, 800)
-        
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        
-        layout = QVBoxLayout(central_widget)
-        
-        # Input section
-        input_group = QGroupBox("Input Stock Symbol")
-        input_layout = QFormLayout()
-        self.stock_input = QLineEdit()
-        self.submit_button = QPushButton("Submit")
-        self.submit_button.clicked.connect(self.on_submit)
-        input_layout.addRow(QLabel("Stock Symbol:"), self.stock_input)
-        input_layout.addRow(self.submit_button)
-        input_group.setLayout(input_layout)
-        layout.addWidget(input_group)
-        
-        # Create a tab widget
-        self.tabs = QtWidgets.QTabWidget()
-        layout.addWidget(self.tabs)
-        
-        # Create tabs for each plot
-        self.train_val_test_tab = QtWidgets.QWidget()
-        self.future_predictions_tab = QtWidgets.QWidget()
-        self.total_revenue_tab = QtWidgets.QWidget()
-        
-        self.tabs.addTab(self.train_val_test_tab, "Train/Val/Test Predictions")
-        self.tabs.addTab(self.future_predictions_tab, "Future Predictions")
-        self.tabs.addTab(self.total_revenue_tab, "Total Revenue")
-        
-        # Layouts for each tab
-        self.train_val_test_layout = QtWidgets.QVBoxLayout(self.train_val_test_tab)
-        self.future_predictions_layout = QtWidgets.QVBoxLayout(self.future_predictions_tab)
-        self.total_revenue_layout = QtWidgets.QVBoxLayout(self.total_revenue_tab)
-        
-    def on_submit(self):
-        Symobl = self.stock_input.text()
-        symbolTicker = yf.Ticker(Symobl)
-        incStat = symbolTicker.income_stmt.transpose()
-        totalRevenue = incStat['Total Revenue']
-        datesIncStat = totalRevenue.index
-        datesIncStat = [str(date.date()) for date in datesIncStat]
-        data = yf.download(Symobl, period="max")
-        
-        # Data preprocessing and model training
-        prediction_days = 100
-        future_prediction_days = 10 
-        now = datetime.datetime.now().date().strftime('%Y-%m-%d')
-        scaled = MinMaxScaler(feature_range=(0, 1))
-        scaled_data = scaled.fit_transform(data['Close'].values.reshape(-1, 1))
 
-        y = data['Close']
-        x = data.drop(['Close'], axis=1)
-        
-        window_df = data_to_window_data(data, '2023-01-01', get_previous_business_day(), n=3)
-        dates, x, y = window_data_to_xy(window_df)
 
-        # Split the data into training, validation, and test sets
-        q80 = int(len(x)*0.90)
-        q90 = int(len(x)*0.95)
 
-        dates_train, x_train, y_train = dates[:q80], x[:q80], y[:q80]
-        dates_val, x_val, y_val = dates[q80:q90], x[q80:q90], y[q80:q90]
-        dates_test, x_test, y_test = dates[q90:], x[q90:], y[q90:]
+# Data visualization
+fig, axd = plt.subplot_mosaic([['upleft', 'right'], ['lowleft', 'right']], layout = 'constrained', figsize=(15, 8))
 
-        # Model training using LSTM
-        model = Sequential([layers.Input((3,1)), layers.LSTM(64), layers.Dense(32, activation = 'relu'), layers.Dense(32, activation = 'relu'), layers.Dense(1)])
-        model.compile(loss='mse', optimizer = Adam(learning_rate = 0.001), metrics = ['mean_absolute_error'])
-        model.fit(x_train, y_train, validation_data = (x_val, y_val), epochs = 150)
+# Plot for training, validation, and test predictions
+axd['upleft'].plot(dates_train, train_pred, c='red')
+axd['upleft'].plot(dates_train, y_train, c='blue')
+axd['upleft'].plot(dates_val, val_pred, c='green')
+axd['upleft'].plot(dates_val, y_val, c='orange')
+axd['upleft'].plot(dates_test, test_pred, c='purple')
+axd['upleft'].plot(dates_test, y_test, c='black')
+axd['upleft'].legend(['Train Prediction', 'Train', 'Validation Prediction', 'Validation', 'Test Prediction', 'Test'], loc = 'upper left')
+axd['upleft'].set_ylabel('Stock Price')
+axd['upleft'].set_xlabel('Date')
+axd['upleft'].set_title('Stock Price Prediction for: ' + Symobl)
 
-        train_pred = model.predict(x_train).flatten()
-        val_pred = model.predict(x_val).flatten()
-        test_pred = model.predict(x_test).flatten()
+# Plot for future predictions
+axd['lowleft'].plot(future_dates, future_predictions, c='teal', linestyle='-')
+axd['lowleft'].set_ylabel('Stock Price for')
+axd['lowleft'].set_xlabel('Date')
+axd['lowleft'].set_title(Symobl + 'Stock Price Prediction for the Next 10 Days')
+axd['lowleft'].tick_params(axis='x', labelsize=8, rotation=45)
+axd['lowleft'].yaxis.set_major_formatter(mticker.ScalarFormatter())
+axd['lowleft'].yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
 
-        # Predict the next 10 days of stock prices
-        last_window = x_test[-1] 
-        future_predictions = predict_future_prices(model, last_window, future_prediction_days)
-        future_prices = predict_future_business_days(future_predictions, future_prediction_days, dates_test)
+# Plot for total revenue over time
+axd['right'].bar(datesIncStat, totalRevenue, color='maroon')
+axd['right'].set_ylabel('Total Revenue')
+axd['right'].set_xlabel('Date')
+axd['right'].set_title('Total Revenue Over Time')
+axd['right'].tick_params(axis='x', labelsize=8, rotation=45)
+axd['right'].yaxis.set_major_formatter(mticker.ScalarFormatter())
+axd['right'].yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+plt.show()
 
-        # Plot for training, validation, and test predictions
-        train_val_test_fig, train_val_test_ax = plt.subplots(figsize=(10, 6))
-        train_val_test_ax.plot(dates_train, train_pred, c='red')
-        train_val_test_ax.plot(dates_train, y_train, c='blue')
-        train_val_test_ax.plot(dates_val, val_pred, c='green')
-        train_val_test_ax.plot(dates_val, y_val, c='orange')
-        train_val_test_ax.plot(dates_test, test_pred, c='purple')
-        train_val_test_ax.plot(dates_test, y_test, c='black')
-        train_val_test_ax.legend(['Train Prediction', 'Train', 'Validation Prediction', 'Validation', 'Test Prediction', 'Test'], loc='upper left')
-        train_val_test_ax.set_ylabel('Stock Price')
-        train_val_test_ax.set_xlabel('Date')
-        train_val_test_ax.set_title('Stock Price Prediction for: ' + Symobl)
-        
-        # Plot for future predictions
-        future_predictions_fig, future_predictions_ax = plt.subplots(figsize=(10, 6))
-        future_predictions_ax.plot(future_prices, future_predictions, c='teal', linestyle='-', marker='o')
-        future_predictions_ax.set_ylabel('Stock Price')
-        future_predictions_ax.set_xlabel('Date')
-        future_predictions_ax.set_title(Symobl + ' Stock Price Prediction for the Next 10 Days')
-        future_predictions_ax.tick_params(axis='x', labelsize=8, rotation=45)
-        future_predictions_ax.yaxis.set_major_formatter(mticker.ScalarFormatter())
-        future_predictions_ax.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-        
-        # Plot for total revenue over time
-        total_revenue_fig, total_revenue_ax = plt.subplots(figsize=(10, 6))
-        total_revenue_ax.bar(datesIncStat, totalRevenue, color='maroon')
-        total_revenue_ax.set_ylabel('Total Revenue')
-        total_revenue_ax.set_xlabel('Date')
-        total_revenue_ax.set_title('Total Revenue Over Time')
-        total_revenue_ax.tick_params(axis='x', labelsize=8, rotation=45)
-        total_revenue_ax.yaxis.set_major_formatter(mticker.ScalarFormatter())
-        total_revenue_ax.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
-        
-        # Add matplotlib figures to the tabs
-        train_val_test_canvas = FigureCanvasQTAgg(train_val_test_fig)
-        future_predictions_canvas = FigureCanvasQTAgg(future_predictions_fig)
-        total_revenue_canvas = FigureCanvasQTAgg(total_revenue_fig)
-        
-        self.train_val_test_layout.addWidget(train_val_test_canvas)
-        self.future_predictions_layout.addWidget(future_predictions_canvas)
-        self.total_revenue_layout.addWidget(total_revenue_canvas)
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec_()
+
